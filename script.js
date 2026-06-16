3229853cc38f45913ee5bedee56cb2b67b3f0e80
Baronsito453 <baronparrabryan@gmail.com>
2026-06-16 17:06:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ $(document).ready(function(){
 
             data.data.forEach(function(partido){
 
+                // Buscamos qué fase es para ponerle su color correspondiente del CSS
+                let claseFase = "fase-grupo"; // Por defecto blanco para Grupos
+
+                if (partido.competition_name.includes("FINAL")) {
+                    claseFase = "fase-final";       // Amarillo/Dorado para la gran final
+                } else if (partido.competition_name.includes("ELIMINATORIAS")) {
+                    claseFase = "fase-eliminatoria"; // Gris elegante para Octavos, Cuartos y Semis
+                }
+
+                // Armamos la fila inyectándole la clase detectada arriba
                 html += `
-                <tr>
+                <tr class="${claseFase}">
                     <td>${partido.home_team}</td>
                     <td>${partido.away_team}</td>
                     <td>${partido.competition_name}</td>
